a641be01cce9e53b8e874825d63a54198bd53874
Noah Kroes <Noah@Noahs-MacBook-Pro.local>
2019-03-13 22:19:14 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,45 +1,40 @@
-TweenMax.from("#txt", 5.5, {
-    opacity: 10,
-    scale: 10,
-    ease: Bounce.easeOut
-});
-
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
 
-    //animate letters
-    animateLetters(h, s);
 
-    // animate background
-    animateBackground(h);
-
-    // Laat tekst zien (klok)
 
     // Voegt een 0 voor cijfers kleiner dan 10
     h = (h < 10) ? '0' + h : h;
     m = (m < 10) ? '0' + m : m;
     s = (s < 10) ? '0' + s : s;
 
+    // Laat tekst zien (klok)
     document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
 
     var t = setTimeout(startTime, 500);
 
 }
 
-function animateLetters(hours, minutes) {
-
-    if (hours == 00 && hours == 01 && hours == 02 && hours == 03 && hours == 04 && hours == 05 && hours == 06 && hours == 07 && hours == 08 && hours == 09 && hours == 10 && hours == 11 && hours == 12 && hours == 13 && hours == 14 && hours == 15 && hours == 16 && hours == 17 && hours == 18 && hours == 19 && hours == 20 && hours == 21 && hours == 22 && hours == 23 && hours == 24 && minutes % 60 == 0) {
-
-    }
-
+if (timeTween.progress() >= 0 && timeTween.progress() < 0.25) {
+    TweenMax.to('html', 2, {
+        backgroundColor: '#000000'
+    });
 }
-
-function animateBackground(hours) {
-
-    if (hours == 12) {
-
-    }
+if (timeTween.progress() >= 0.25 && timeTween.progress() < 0.5) {
+    TweenMax.to('html', 2, {
+        backgroundColor: '#e09572'
+    });
+}
+if (timeTween.progress() >= 0.5 && timeTween.progress() < 0.75) {
+    TweenMax.to('html', 2, {
+        backgroundColor: '#e0bfb0'
+    });
+}
+if (timeTween.progress() >= 0.75 && timeTween.progress() < 1) {
+    TweenMax.to('html', 2, {
+        backgroundColor: '#94624b'
+    });
 }
