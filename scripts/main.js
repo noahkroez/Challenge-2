c154b246d022185f4bce8bce6927719c34225515
Noah Kroes <Noah@Noahs-MacBook-Pro.local>
2019-03-14 09:52:45 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,34 @@
+//function for js clock
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
 
-
-
-    // Voegt een 0 voor cijfers kleiner dan 10
+    // Adds leading zero 0
     h = (h < 10) ? '0' + h : h;
     m = (m < 10) ? '0' + m : m;
     s = (s < 10) ? '0' + s : s;
 
-    // Laat tekst zien (klok)
+    // shows clock
     document.getElementById('txt').innerHTML = h + ":" + m + ":" + s;
 
-    var t = setTimeout(startTime, 500);
+    setTimeout(startTime, 500);
+}
+//Background color changing gradually over current time
+var now = new Date(),
+    hour = now.getHours(),
+    color = "#000";
 
+if (hour >= 6 && hour < 12) {
+    color = "#e09572";
+} else if (hour >= 12 && hour < 18) {
+    color = "#e0bfb0";
+} else if (hour >= 18) {
+    color = "#94624b";
 }
+TweenMax.to("html", 2, {
+    backgroundColor: color
+});
 
-if (timeTween.progress() >= 0 && timeTween.progress() < 0.25) {
-    TweenMax.to('html', 2, {
-        backgroundColor: '#000000'
-    });
-}
-if (timeTween.progress() >= 0.25 && timeTween.progress() < 0.5) {
-    TweenMax.to('html', 2, {
-        backgroundColor: '#e09572'
-    });
-}
-if (timeTween.progress() >= 0.5 && timeTween.progress() < 0.75) {
-    TweenMax.to('html', 2, {
-        backgroundColor: '#e0bfb0'
-    });
-}
-if (timeTween.progress() >= 0.75 && timeTween.progress() < 1) {
-    TweenMax.to('html', 2, {
-        backgroundColor: '#94624b'
-    });
-}
+startTime();
